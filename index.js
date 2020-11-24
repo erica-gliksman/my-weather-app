eecc865dfc667e3c5c686b580fa775220cf2ecee
erica-gliksman <70657328+erica-gliksman@users.noreply.github.com>
2020-11-24 14:25:55 -0500

    many more changes

--- index.js
+++ index.js
@@ -32,7 +32,8 @@ function displayWeatherCondition(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
-  
+  iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`); 
+iconElement.setAttribute("alt", response.data.weather[0].description);
   
 }
 
@@ -53,5 +54,3 @@ let iconElement = document.querySelector("#icon");
 searchForm.addEventListener("submit", search);
 
 dateElement.innerHTML = formatDate(currentTime);
-iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`); 
-iconElement.setAttribute("alt", response.data.weather[0].description);
