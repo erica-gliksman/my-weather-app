c897315c044f48429d97e1ed01e37a0fafa5fcc0
erica-gliksman <70657328+erica-gliksman@users.noreply.github.com>
2020-11-26 11:36:07 -0500

    Fixed conversion problem

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ function displayWeatherCondition(response) {
   );
   iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`); 
 iconElement.setAttribute("alt", response.data.weather[0].description);
+celsiusTemperature = response.data.main.temp;
   
 }
 
